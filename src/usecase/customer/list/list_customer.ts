e91c22e43ea6b01e8f278448bcf03821ea906030
Leo Silva <leocardosodev@gmail.com>
2023-09-25 14:11:21 -0300

    feat: add list all  use case to Customer

---
+++ src/usecase/customer/list/list_customer.ts
@@ -0,0 +1,31 @@
+import Customer from "../../../domain/customer/entity/customer";
+import CustomerRepositoryInterface from "../../../domain/customer/repository/customer_repository_interface";
+import { InputListCustomerDto, OutputListCustomerDto } from "./list_customer_dto";
+
+export default class ListCustomerUseCase {
+  private customerRepository: CustomerRepositoryInterface;
+  constructor(customerRepository: CustomerRepositoryInterface) {
+    this.customerRepository = customerRepository;
+  }
+  async execute(input: InputListCustomerDto): Promise<OutputListCustomerDto> {
+    const customers = await this.customerRepository.findAll();
+    return OutputMapper.toOutput(customers);
+  }
+}
+
+class OutputMapper {
+  static toOutput(customer: Customer[]): OutputListCustomerDto {
+    return {
+      customers: customer.map((customer) => ({
+        id: customer.id,
+        name: customer.name,
+        address: {
+          street: customer.address.street,
+          number: customer.address.number,
+          zip: customer.address.zip,
+          city: customer.address.city,
+        }
+      })),
+    }
+  }
+}
---
+++ src/usecase/customer/list/list_customer_unit.spec.ts
@@ -0,0 +1,38 @@
+import Customer from "../../../domain/customer/entity/customer";
+import Address from "../../../domain/customer/value_object/address";
+import ListCustomerUseCase from "./list_customer";
+
+const customer1 = new Customer("1", "John Doe");
+const address1 = new Address("Street 1", 1, "11111-111", "City 1");
+customer1.changeAddress(address1);
+
+const customer2 = new Customer("2", "Jane Doe");
+const address2 = new Address("Street 2", 2, "22222-222", "City 2");
+customer2.changeAddress(address2);
+
+const MockRepository = () => {
+  return {
+    find: jest.fn(),
+    findAll: jest.fn().mockReturnValue(Promise.resolve([customer1, customer2])),
+    create: jest.fn(),
+    update: jest.fn(),
+  }
+}
+
+describe("Unit test for list customer use case", () => {
+  it("Should list a customer", async () => {
+    const customerRepository = MockRepository();
+    const useCase = new ListCustomerUseCase(customerRepository);
+    const output = await useCase.execute({});
+
+    expect(output.customers.length).toBe(2);
+    expect(output.customers[0].id).toBe(customer1.id);
+    expect(output.customers[0].name).toBe(customer1.name);
+    expect(output.customers[0].address.street).toBe(customer1.address.street);
+
+    expect(output.customers[1].id).toBe(customer2.id);
+    expect(output.customers[1].name).toBe(customer2.name);
+    expect(output.customers[1].address.street).toBe(customer2.address.street);
+
+  });
+});
