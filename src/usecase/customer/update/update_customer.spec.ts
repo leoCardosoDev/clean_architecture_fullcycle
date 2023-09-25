a139e7df31618a6356bf32ec79f7236470e6a8ce
Leo Silva <leocardosodev@gmail.com>
2023-09-25 10:59:15 -0300

    feat: add update use case to Customer

---
+++ src/usecase/customer/update/update_customer.spec.ts
@@ -0,0 +1,34 @@
+import CustomerFactory from "../../../domain/customer/factory/customer_factory"
+import Address from "../../../domain/customer/value_object/address"
+import UpdateCustomerUseCase from "./update_customer";
+
+const customer = CustomerFactory.createWithAddress("Leo", new Address("Street 1", 23, "Zip", "City"));
+const input = {
+  id: customer.id,
+  name: "Leo Updated",
+  address: {
+    street: "Street Updated",
+    number: 2,
+    zip: "Zip Updated",
+    city: "City Updated"
+  }
+}
+
+const MockRepository = () => {
+  return {
+    find: jest.fn().mockReturnValue(Promise.resolve(customer)),
+    findAll: jest.fn(),
+    create: jest.fn(),
+    update: jest.fn(),
+  }
+}
+
+describe("Unit test for customer update use case", () => {
+  it("Should update a customer", async () => {
+    const customerRepository = MockRepository();
+    const customerUpdateUseCase = new UpdateCustomerUseCase(customerRepository);
+    const output = await customerUpdateUseCase.execute(input);
+    expect(output).toEqual(input)
+  });
+});
+
---
+++ src/usecase/customer/update/update_customer.ts
@@ -0,0 +1,35 @@
+import CustomerRepositoryInterface from "../../../domain/customer/repository/customer_repository_interface";
+import Address from "../../../domain/customer/value_object/address";
+import { OutputCreateCustomerDto } from "../create/create_customer_dto";
+import { InputUpdateCustomerDto } from "./update_customer_dto";
+
+export default class UpdateCustomerUseCase {
+  private customerRepository: CustomerRepositoryInterface;
+  constructor(customerRepository: CustomerRepositoryInterface) {
+    this.customerRepository = customerRepository;
+  }
+
+ async execute(input: InputUpdateCustomerDto): Promise<OutputCreateCustomerDto> {
+  const customer = await this.customerRepository.find(input.id);
+  customer.changeName(input.name);
+  customer.changeAddress(
+    new Address(
+      input.address.street,
+      input.address.number,
+      input.address.zip,
+      input.address.city
+    )
+  );
+  await this.customerRepository.update(customer);
+  return {
+    id: customer.id,
+    name: customer.name,
+    address: {
+      street: customer.address.street,
+      number: customer.address.number,
+      zip: customer.address.zip,
+      city: customer.address.city,
+    }
+  };
+ }
+}
