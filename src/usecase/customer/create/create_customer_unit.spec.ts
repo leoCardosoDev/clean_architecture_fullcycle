01cda927cb173445be0ace85bddf3f0c9ebda612
Leo Silva <leocardosodev@gmail.com>
2023-09-25 10:21:21 -0300

    feat: add throw if address or name i'nst provided

--- src/usecase/customer/create/create_customer_unit.spec.ts
+++ src/usecase/customer/create/create_customer_unit.spec.ts
@@ -35,4 +35,18 @@ describe("Unit test create Customer", () => {
       }
     })
   });
+
+  it("Should throw error when name is missing", async () => {
+    const customerRepository = MockRepository();
+    const customerCreateUseCase = new CustomerCreateUseCase(customerRepository);
+    input.name = "";
+    await expect(customerCreateUseCase.execute(input)).rejects.toThrow("Name is required");
+  });
+
+  it("Should throw error when street is missing", async () => {
+    const customerRepository = MockRepository();
+    const customerCreateUseCase = new CustomerCreateUseCase(customerRepository);
+    input.address.street = "";
+    await expect(customerCreateUseCase.execute(input)).rejects.toThrow("Street is required");
+  });
 });
